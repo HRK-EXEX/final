a1c6fb14fcc4dc9a6a89b6ca3164521b3f0362cf
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-01-10 11:45:48 +0900

    DB挿入完了

--- src/index.php
+++ src/index.php
@@ -1,1 +1,14 @@
-仮置きです
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>トレ「ガ」ジャー - ログイン</title>
+    </head>
+    <body>
+        <div id="box">
+            <p>アカウントID</p>
+            <p>パスワード</p>
+        </div>
+    </body>
+</html>
--- src/initial/sql.sql
+++ src/initial/sql.sql
@@ -1,34 +1,77 @@
-CREATE TABLE Accounts IF NOT EXISTS (
+DROP TABLE IF EXISTS HasItems, Treasures, Histories, Accounts, Categories, Items;
+
+CREATE TABLE IF NOT EXISTS Accounts (
     account_id    VARCHAR(32)  PRIMARY KEY,
     account_nick  VARCHAR(128) NOT NULL,
     account_pswd  VARCHAR(128) NOT NULL,
-    account_level INT          NOT NULL,
+    account_level INTEGER      NOT NULL,
     account_point BIGINT       NOT NULL,
-    account_rank  INT          NOT NULL
+    account_rank  INTEGER      NOT NULL
 );
 
-CREATE TABLE Categories IF NOT EXISTS (
-    category_id    INT PRIMARY KEY AUTO_INCREMENT,
-    category_name  VARCHAR(128)
+CREATE TABLE IF NOT EXISTS Categories (
+    category_id   INTEGER PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(128)
 );
 
-CREATE TABLE Treasures IF NOT EXISTS (
-    treasure_id    CHAR(32)      PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Items  (
+    item_id    INTEGER PRIMARY KEY AUTO_INCREMENT,
+    item_name  VARCHAR(128),
+    item_desc  VARCHAR(128),
+    item_point VARCHAR(128)
+);
+
+CREATE TABLE IF NOT EXISTS HasItems (
+    account_id VARCHAR(32),
+    item_id    INTEGER,
+    PRIMARY KEY (account_id, item_id),
+    FOREIGN KEY (account_id) REFERENCES Accounts (account_id),
+    FOREIGN KEY (item_id) REFERENCES Items (item_id)
+);
+
+CREATE TABLE IF NOT EXISTS Treasures (
+    treasure_id    INTEGER       PRIMARY KEY AUTO_INCREMENT,
     treasure_name  VARCHAR(128)  NOT NULL,
     treasure_desc  VARCHAR(128)  NOT NULL,
-    treasure_ctgr  VARCHAR(128)  NOT NULL,
-    treasure_rank  INT           NOT NULL,
-    treasure_price DECIMAL(64,0) NOT NULL UNSIGNED,
-    FOREIGN KEY (treasure_ctgr) REFERENCES Categories(category_id)
+    treasure_rank  INTEGER       NOT NULL,
+    treasure_price DECIMAL(64,0) NOT NULL,
+    treasure_ctgr  INTEGER       NOT NULL,
+    FOREIGN KEY (treasure_ctgr) REFERENCES Categories (category_id)
 );
 
-CREATE TABLE Histories IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS Histories (
     account_id  CHAR(32),
-    treasure_id CHAR(32),
-    PRIMARY KEY(account_id, treasure_id),
-    FOREIGN KEY (account_id)  REFERENCES accounts (account_id),
+    treasure_id INTEGER,
+    PRIMARY KEY (account_id, treasure_id),
+    FOREIGN KEY (account_id)  REFERENCES Accounts (account_id),
     FOREIGN KEY (treasure_id) REFERENCES Treasures (treasure_id)
 );
 
--- INSERT IGNORE INTO Customer VALUES
---     (NULL, 'Administrator', 'WHERE DO U LIVE? IDK', 'Admin', 'Admin');
+INSERT INTO Accounts VALUE ('必殺奥義！ッ管理者権限!!ッ', 'Administrator', '¯\_(ツ)_/¯', 1, 0, 0);
+
+INSERT INTO Categories (category_name) VALUES
+    ('日用品'),
+    ('高級品'),
+    ('インテリア'),
+    ('ハードウェア'),
+    ('家電機器'),
+    ('建物'),
+    ('概念'),
+    ('惑星')
+;
+
+INSERT INTO Items (item_name, item_desc, item_point) VALUES
+    ('挑戦券', 'ゲーム開始前に１枚必要。', 10),
+    ('運気券', '宝が当たりやすくなる。', 15),
+    ('超運券', '必ず宝が当たるようになる。', 50),
+    ('豪運券', '必ずレアな宝が当たるようになる。(Aランク以上)', 200),
+    ('欲張り屋', '全ての宝箱を開けられる。', 80),
+    ('Ｘ視線', '宝箱の中身を見ることができる。', 100),
+    ('権限の暴力', '入手したい宝を選ぶことができる。', 9999)
+;
+
+INSERT INTO Treasures (treasure_name, treasure_desc, treasure_ctgr, treasure_rank, treasure_price) VALUES 
+    ('テストデータ１', 'テストですよ', 6, 0, 10),
+    ('テストデータ２', 'テストですよよ', 6, 0, 100),
+    ('テストデータ３', 'テストですよよよ', 6, 0, 1000)
+;
