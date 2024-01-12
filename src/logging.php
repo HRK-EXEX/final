2db2c45ba6b8baf0bbc0d913502cf03a596b4d2d
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-01-12 12:43:42 +0900

    修正?

--- src/logging.php
+++ src/logging.php
@@ -6,17 +6,19 @@
     $pswd = $_POST['pswd'];
     $exp = 0;
 
-    $sql = $db -> prepare("SELECT * FROM Accounts WHERE accounts_id = '?'");
-    $sql -> execute([$id]);
+    $sql = $db -> prepare("SELECT * FROM Accounts WHERE account_id = ?");
+    $sql -> execute(["'".$id."'"]);
 
     $res = $sql -> fetch(PDO::FETCH_ASSOC);
-    if($res['account_pswd'] == $pswd){
-        $_COOKIE['loginfo'] = [
-            'acc_id' -> $id,
-            'pswd' -> $pswd
-        ];
-        header('Location: index.php');
+    if($res) {
+        if($res['account_pswd'] == $pswd){
+            $_COOKIE['loginfo'] = [
+                'acc_id' -> $id,
+                'pswd' -> $pswd
+            ];
+            header('Location: index.php');
+        }
     } else {
-        header('Location: login.php?err=1');
+        header('Location: login.php?err=1', true, 307);
     }
 ?>
--- src/login.php
+++ src/login.php
@@ -9,19 +9,28 @@
     <body>
         <div class="master-box">
             <div>
-                <?php
-                    $err = $_GET['err'] ?? null;
-                    switch ($err) {
-                        case '1':
-                            echo 'アカウント名またはパスワードが一致しません。';
-                            break;
-                    }
-                ?>
                 <form action="logging.php" method="post">
-                    <img src="img/title.png" alt="トレ「ガ」ヂャー"><br><br>
-                    <input class="text" name="id" type="text" required placeholder="アカウントID"><br>
-                    <input class="text" name="pswd" type="password" required placeholder="パスワード"><br>
-                    <button>入場する</button><br>
+                    <img src="img/title.png" alt="トレ「ガ」ヂャー"><br>
+                    
+                    <?php
+                        $id = $_POST['id'] ?? null;
+                        $pswd = $_POST['pswd'] ?? null;
+
+                        $err = $_GET['err'] ?? null;
+                        switch ($err) {
+                            case '1':
+                                echo 'アカウント名またはパスワードが一致しません。<br>';
+                                break;
+                            case '2':
+                                echo '謎のエラーです。<br>';
+                                break;
+                        }
+                    ?>
+                    <br>
+
+                    <input class="text" name="id" type="text" required placeholder="アカウントID" value="<?=$id?>"><br>
+                    <input class="text" name="pswd" type="password" required placeholder="パスワード" value="<?=$pswd?>"><br>
+                    <button type="submit">入場する</button><br>
                 </form>
 
                 <p><br>↓アカウントをお持ちでない方↓</p>
